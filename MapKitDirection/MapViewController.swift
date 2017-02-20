035d7851908dc8419fedc157ed6ca57185e23d5b
355 Berry Street <p.mateo@355berrystreet.com>
2017-02-20 01:29:24 +0100

    Initial Commit

---
+++ MapKitDirection/MapViewController.swift
@@ -0,0 +1,95 @@
+//
+//  MapViewController.swift
+//  MapKitDirection
+//
+//  Created by Pablo Mateo Fernández on 27/01/2017.
+//  Copyright © 2017 355 Berry Street S.L. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var myViewTest: UIView!
+    
+    
+    
+    var restaurant:Restaurant!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+        // Convert address to coordinate and annotate it on map
+        let geoCoder = CLGeocoder()
+        geoCoder.geocodeAddressString(restaurant.location, completionHandler: { placemarks, error in
+            if error != nil {
+                print(error)
+                return
+            }
+            
+            if let placemarks = placemarks {
+                // Get the first placemark
+                let placemark = placemarks[0]
+                
+                // Add annotation
+                let annotation = MKPointAnnotation()
+                annotation.title = self.restaurant.name
+                annotation.subtitle = self.restaurant.type
+                
+                if let location = placemark.location {
+                    annotation.coordinate = location.coordinate
+                    
+                    // Display the annotation
+                    self.mapView.showAnnotations([annotation], animated: true)
+                    self.mapView.selectAnnotation(annotation, animated: true)
+                }
+            }
+            
+        })
+        
+        mapView.delegate = self
+        if #available(iOS 9.0, *) {
+            mapView.showsCompass = true
+            mapView.showsScale = true
+            mapView.showsTraffic = true
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - MKMapViewDelegate methods
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        let identifier = "MyPin"
+        
+        if annotation.isKind(of: MKUserLocation.self) {
+            return nil
+        }
+        
+        // Reuse the annotation if possible
+        var annotationView:MKPinAnnotationView? = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
+        
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+            annotationView?.canShowCallout = true
+        }
+        
+        let leftIconView = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 53, height: 53))
+        leftIconView.image = UIImage(named: restaurant.image)
+        annotationView?.leftCalloutAccessoryView = leftIconView
+        
+        // Pin color customization
+        if #available(iOS 9.0, *) {
+            annotationView?.pinTintColor = UIColor.orange
+        }
+        
+        return annotationView
+    }
+    
+}
---
+++ MapKitDirection/Restaurant.swift
@@ -0,0 +1,28 @@
+//
+//  File.swift
+//  MapKitDirection
+//
+//  Created by Pablo Mateo Fernández on 27/01/2017.
+//  Copyright © 2017 355 Berry Street S.L. All rights reserved.
+//
+
+import Foundation
+
+class Restaurant {
+    var name = ""
+    var type = ""
+    var location = ""
+    var image = ""
+    var isVisited = false
+    var phone = ""
+    var rating = ""
+    
+    init(name: String, type: String, location: String, phone: String, image: String, isVisited: Bool) {
+        self.name = name
+        self.type = type
+        self.location = location
+        self.phone = phone
+        self.image = image
+        self.isVisited = isVisited
+    }
+}
